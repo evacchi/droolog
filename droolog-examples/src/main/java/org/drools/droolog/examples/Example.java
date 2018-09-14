6454bea3935fac64fa29de96db75c42b58bc046a
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-09-14 15:26:29 +0200

    example

--- droolog-examples/src/main/java/org/drools/droolog/examples/Example.java
+++ droolog-examples/src/main/java/org/drools/droolog/examples/Example.java
@@ -19,16 +19,31 @@ public class Example {
         PersonMeta person = PersonMeta.Instance;
         AddressMeta address = AddressMeta.Instance;
 
-        Term.Structure<Person> p1 = person.of(first, variable(), variable());
-        Term.Structure<Person> p2 = person.of(variable(), second, address.of(street, city, zip, country));
+        {
+            Term.Structure<Person> p1 = person.of(first, variable(), variable());
+            Term.Structure<Person> p2 = person.of(variable(), second, address.of(street, city, zip, country));
 
-        Unification<Person> unification = Unification.of(p1, p2);
-        Term.Structure<Person> s = unification.term();
-        Person p = person.of(s);
+            Unification<Person> unification = Unification.of(p1, p2);
+            Term.Structure<Person> s = unification.term();
+            Person p = person.of(s);
 
-        System.out.println(s);
-        System.out.println(p);
-        System.out.println(unification.bindings());
+            System.out.println(s);
+            System.out.println(p);
+            System.out.println(unification.bindings());
+        }
+
+        {
+            Term.Structure<Person> p1 = person.of(first, variable(), variable());
+            Term.Structure<Person> p2 = person.of(variable(), second, variable());
+
+            Unification<Person> unification = Unification.of(p1, p2);
+            Term.Structure<Person> s = unification.term();
+            Person p = person.of(s);
+
+            System.out.println(s);
+            System.out.println(p);
+            System.out.println(unification.bindings());
+        }
 
     }
 
